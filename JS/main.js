f7f46b96d82609fd91d14341cd7bb9bc499cd99a
Kuznetsova94 <67519395+Kuznetsova94@users.noreply.github.com>
2020-06-30 16:48:03 +0400

    All files for my final project

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+/**
+* Template Name: Kelly - v2.0.0
+* Template URL: https://bootstrapmade.com/kelly-free-bootstrap-cv-resume-html-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+/* Home page*/  
+new TypeIt("#myElement", {
+    speed: 50,
+    waitUntilVisible: true
+  })
+    .type("I'm logistic manager", { delay: 300 })
+    
+    .delete(16)
+    .type('future web-developer')
+    .move(-9)
+    .delete (4)
+    .type('front-end ')
+    .pause(300)
+    .move(9)
+    .type('!!!')
+    .go();
+
+
+
+        
